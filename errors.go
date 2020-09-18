e47717b09c4f6e81ef8a08651c2c561960c79f0a
Brendan Bosman <bhbosman@gmail.com>
2020-09-18 08:42:52 +0200

    WIP

---
+++ errors.go
@@ -0,0 +1,15 @@
+package goerrors
+
+import "errors"
+
+var NotImplemented error = errors.New("not implemented")
+var InvalidParam error = errors.New("invalid param")
+var TimeOut error = errors.New("invalid param")
+var InvalidType error = errors.New("not implemented")
+var InvalidSignature error = errors.New("InvalidSignature")
+var InvalidState error = errors.New("InvalidState")
+var InvalidSequenceNumber error = errors.New("InvalidSequenceNumber")
+var DuplicateKey error = errors.New("DuplicateKey")
+
+const BottomStackName = "BottomStack"
+const TopStackName = "TopStack"
